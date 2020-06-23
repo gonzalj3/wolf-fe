7b1fca8e143b966dd09c53722646510dd0c1176b
gonzalj3 <martin.jose.gonzalez@gmail.com>
2020-06-23 03:39:58 -0500

    Connected registration and log in to backend.

--- src/components/dialog-CreateAccount.js
+++ src/components/dialog-CreateAccount.js
@@ -7,6 +7,8 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Checkbox from "@material-ui/core/Checkbox";
 import FormLabel from "@material-ui/core/FormLabel";
+import { FormHelperText } from "@material-ui/core";
+import { useForm } from "react-hook-form";
 
 import Link from "@material-ui/core/Link";
 import { makeStyles } from "@material-ui/core/styles";
@@ -29,9 +31,16 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function LogIn({ buttonTitle }) {
+  const { register, handleSubmit } = useForm();
   const classes = useStyles();
 
   const [open, setOpen] = React.useState(false);
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [firstName, setFirstName] = React.useState("");
+  const [lastName, setLastName] = React.useState("");
+  const [schoolName, setSchoolName] = React.useState("");
+  const [serverResponse, setServerResponse] = React.useState("");
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -39,6 +48,52 @@ export default function LogIn({ buttonTitle }) {
 
   const handleClose = () => {
     setOpen(false);
+  };
+
+  const onSubmit = () => {
+    console.log("info getting conveyed");
+    //const { firstName, lastName, schoolName, email, password, thing } = values;
+    //console.log(thing);
+    console.log(email, password, firstName, lastName, email, schoolName);
+    //Make a request to backend
+    const url = "http://localhost:4000/api/register";
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        firstName,
+        lastName,
+        schoolName,
+        email,
+        password,
+      }),
+    };
+    fetch(url, options)
+      .then((res) => res.json())
+      .then((res) => {
+        //If success to create a new account, redirect to login page
+        if (!res.error) {
+          //Save data on local storage
+          localStorage.setItem("isAuthenticated", true);
+          localStorage.setItem("user", JSON.stringify(res.user));
+          localStorage.setItem("token", JSON.stringify(res.token));
+
+          //Update the state of Auth providers
+          /*dispatchIsAuthenticated(setIsAuthenticated(true));
+          dispatchUser(fetchUserSuccess(res.user));*/
+          console.log("login successfully");
+
+          //Redirect to dashboard
+          window.location.replace("/dashboard");
+        } else {
+          throw Error(res.error);
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+        /*dispatchUser(fetchUserFailure(e.message));*/
+        setServerResponse(e.message);
+      });
   };
 
   return (
@@ -52,75 +107,95 @@ export default function LogIn({ buttonTitle }) {
         onClose={handleClose}
         aria-labelledby="form-dialog-title"
       >
-        <DialogTitle id="form-dialog-title">
-          {`Create ` + buttonTitle + ` Account`}
-        </DialogTitle>
-        <DialogContent>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="First Name"
-            type="string"
-            fullWidth
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Last Name"
-            type="string"
-            fullWidth
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="School Name"
-            type="string"
-            fullWidth
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
-            fullWidth
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Password"
-            type="password"
-            fullWidth
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Verify Password"
-            type="password"
-            fullWidth
-          />
-          <div className={classes.termsBox}>
-            <FormLabel>
-              {
-                <div>
-                  <span>I agree to the </span>
-                  <Link href="/terms">terms</Link>
-                </div>
-              }
-            </FormLabel>
-            <Checkbox color="primary" />
-          </div>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            submit
-          </Button>
-        </DialogActions>
+        <form>
+          <DialogTitle id="form-dialog-title">
+            {`Create ` + buttonTitle + ` Account`}
+          </DialogTitle>
+          <FormHelperText error={true} className={classes.error}>
+            {serverResponse}
+          </FormHelperText>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="firstName"
+              name="firstName"
+              label="First Name"
+              type="string"
+              inputRef={register}
+              onInput={(e) => setFirstName(e.target.value)}
+              fullWidth
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="lastName"
+              name="lastName"
+              label="Last Name"
+              type="string"
+              inputRef={register}
+              onInput={(e) => setLastName(e.target.value)}
+              fullWidth
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="schoolName"
+              name="schoolName"
+              label="School Name"
+              type="string"
+              inputRef={register}
+              onInput={(e) => setSchoolName(e.target.value)}
+              fullWidth
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="email"
+              name="email"
+              label="Email Address"
+              type="email"
+              ref={register}
+              onInput={(e) => setEmail(e.target.value)}
+              fullWidth
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="password"
+              name="password"
+              label="Password"
+              type="password"
+              inputRef={register}
+              onInput={(e) => setPassword(e.target.value)}
+              fullWidth
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Verify Password"
+              type="password"
+              fullWidth
+            />
+            <div className={classes.termsBox}>
+              <FormLabel>
+                {
+                  <div>
+                    <span>I agree to the </span>
+                    <Link href="/terms">terms</Link>
+                  </div>
+                }
+              </FormLabel>
+              <Checkbox color="primary" />
+            </div>
+          </DialogContent>
+          <DialogActions>
+            <Button color="primary" onClick={onSubmit}>
+              submit
+            </Button>
+          </DialogActions>
+        </form>
       </Dialog>
     </div>
   );
--- src/components/dialog-LogIn.js
+++ src/components/dialog-LogIn.js
@@ -6,6 +6,7 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import { makeStyles } from "@material-ui/core/styles";
+import { FormHelperText } from "@material-ui/core";
 
 import CreateAccount from "../components/dialog-CreateAccount";
 
@@ -20,8 +21,10 @@ const useStyles = makeStyles({
 
 export default function LogIn({ buttonTitle }) {
   const classes = useStyles();
-
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
   const [openLogIn, setOpen] = React.useState(false);
+  const [serverResponse, setServerResponse] = React.useState("");
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -29,6 +32,49 @@ export default function LogIn({ buttonTitle }) {
 
   const handleClose = () => {
     setOpen(false);
+  };
+
+  const onSubmit = () => {
+    console.log("info getting conveyed");
+    //const { firstName, lastName, schoolName, email, password, thing } = values;
+    //console.log(thing);
+    console.log(email, password);
+    //Make a request to backend
+    const url = "http://localhost:4000/api/login";
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    };
+    fetch(url, options)
+      .then((res) => res.json())
+      .then((res) => {
+        //If success to create a new account, redirect to login page
+        if (!res.error) {
+          //Save data on local storage
+          localStorage.setItem("isAuthenticated", true);
+          localStorage.setItem("user", JSON.stringify(res.user));
+          localStorage.setItem("token", JSON.stringify(res.token));
+
+          //Update the state of Auth providers
+          /*dispatchIsAuthenticated(setIsAuthenticated(true));
+          dispatchUser(fetchUserSuccess(res.user));*/
+          console.log("login successfully");
+
+          //Redirect to dashboard
+          window.location.replace("/dashboard");
+        } else {
+          throw Error(res.error);
+        }
+      })
+      .catch((e) => {
+        console.log(e.message);
+        /*dispatchUser(fetchUserFailure(e.message));*/
+        setServerResponse(e.message);
+      });
   };
 
   return (
@@ -48,22 +94,29 @@ export default function LogIn({ buttonTitle }) {
           <TextField
             autoFocus
             margin="dense"
-            id="name"
+            id="email"
+            name="email"
             label="Email Address"
             type="email"
+            onInput={(e) => setEmail(e.target.value)}
             fullWidth
           />
           <TextField
             autoFocus
             margin="dense"
-            id="name"
+            id="password"
+            name="password"
             label="Password"
             type="password"
+            onInput={(e) => setPassword(e.target.value)}
             fullWidth
           />
+          <FormHelperText error={true} className={classes.error}>
+            {serverResponse}
+          </FormHelperText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose} color="primary">
+          <Button onClick={onSubmit} color="primary">
             log in
           </Button>
           <CreateAccount buttonTitle={buttonTitle}>
