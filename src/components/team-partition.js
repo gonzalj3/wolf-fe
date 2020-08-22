f9798b0d63fc1db34b698dfcb5d3fb9a7ee00f74
gonzalj3 <martin.jose.gonzalez@gmail.com>
2020-08-22 04:35:44 -0500

    Set up add team.

--- src/components/team-partition.js
+++ src/components/team-partition.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core";
 import Team from "../components/team";
-import initialData from "../data/initial-data";
+import { IconButton } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import { yellow } from "@material-ui/core/colors";
 
 const useStyle = makeStyles((theme) => ({
   container: {
@@ -14,11 +16,62 @@ const useStyle = makeStyles((theme) => ({
     justifyContent: "center",
   },
 }));
+
 export default function TeamPartition(props) {
   const classes = useStyle();
+  const addTeam = () => {
+    console.log("we are adding a team.");
+    if (props.socket) {
+      //console.log("the gamecode is :", props.data.gameCode);
+      //console.log("we also have a socket: ", props.socket);
+      let lastTeamInd = props.data.TeamOrder.length;
+      let team;
+      console.log("length of team: ", lastTeamInd);
+      switch (lastTeamInd) {
+        case 1:
+          team = "yellow";
+          break;
+        case 2:
+          team = "red";
+          break;
+        case 3:
+          team = "green";
+          break;
+        case 4:
+          team = "purple";
+          break;
+        case 5:
+          team = "orange";
+      }
+      props.socket.emit("newTeam", {
+        room: props.data.gameCode,
+        team: team,
+      }); //on(props.data.gameCode).;
+    } else {
+      console.log("we do not have any socket: ", props.socket);
+    }
+  };
+
+  function AddTeamFunctionality(props) {
+    console.log("************ isTeacher:", props.isTeacher);
+
+    if (props.isTeacher) {
+      return (
+        <div>
+          <IconButton onClick={addTeam}>
+            <AddIcon></AddIcon>
+          </IconButton>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
 
   return (
     <div className={classes.container}>
+      <AddTeamFunctionality isTeacher={props.isTeacher}></AddTeamFunctionality>
+
       {props.data.TeamOrder.map((item) => {
         const team = props.data.droppable[item];
         console.log("rosterList : ", team.students);
