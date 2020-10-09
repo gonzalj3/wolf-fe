1ef2d45e40b136235ee2ed1ab6f31724c448586f
gonzalj3 <martin.jose.gonzalez@gmail.com>
2020-10-09 02:45:28 -0500

    fixing websocket problem

--- src/pages/gameBoard.js
+++ src/pages/gameBoard.js
@@ -23,7 +23,7 @@ const useStyle = makeStyles((theme) => ({
     backgroundColor: "#D3D3D3",
   },
 }));
-const socket = socketIOClient("ws://wolfgamebetabe.herokuapp.com/game");
+const socket = socketIOClient("ws://wolfgamebetabe.herokuapp.com:5000/game");
 export default function GameBoard() {
   const classes = useStyle();
 
