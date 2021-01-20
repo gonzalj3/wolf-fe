c6e61c9f0366341bed72b90ca698aef0038d5812
gonzalj3 <martin.jose.gonzalez@gmail.com>
2021-01-20 12:00:00 -0600

    Have set up listing the number of answers submitted versus number of students along with a nicer looking tableresponses

--- src/components/studentResponses.js
+++ src/components/studentResponses.js
@@ -1,18 +1,23 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { GameInfoContext } from '../context/GameInfoContext.js';
-import { makeStyles } from '@material-ui/core';
+import React, { useContext, useEffect, useState } from "react";
+import { GameInfoContext } from "../context/GameInfoContext.js";
+import {
+  makeStyles,
+  TableContainer,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@material-ui/core";
 
 const useStyle = makeStyles((theme) => ({
   tableContainer: {
-    marginTop: '100px',
-    width: '100%',
-    border: 'solid 1px black',
-    borderCollapse: 'collapse',
-    background: 'white',
+    marginTop: "50px",
   },
   table: {
-    border: 'solid 1px black',
-    borderCollapse: 'collapse',
+    border: "solid 1px black",
+    borderCollapse: "collapse",
   },
 }));
 
@@ -20,42 +25,46 @@ export default function StudentResponse() {
   const classes = useStyle();
   const gameInfo = useContext(GameInfoContext);
 
-  console.log('gameInfo : ', gameInfo);
+  console.log("gameInfo : ", gameInfo);
 
   if (gameInfo.isTeacher && gameInfo.responses) {
     return (
-      <table className={classes.tableContainer}>
-        <tbody>
-          <tr className={classes.table}>
-            <th key={''} className={classes.table}>
-              Name
-            </th>
-            <th className={classes.table}>Team</th>
-            <th className={classes.table}>Answer</th>
-          </tr>
-          {gameInfo.responses.map((student) => (
-            <tr key={`${student.name}${student.team}`}>
-              <td
-                key={`${student.name}`}
-                className={classes.table}
-              >{`${student.name}`}</td>
-              <td
-                key={`${student.team}`}
-                className={classes.table}
-              >{`${student.team}`}</td>
-              <td
-                key={`${student.response}`}
-                className={classes.table}
-              >{`${student.response}`}</td>
-            </tr>
-          ))}
-          <tr className={classes.table}>
-            <td className={classes.table}>{`.`}</td>
-            <td className={classes.table}></td>
-            <td className={classes.table}></td>
-          </tr>
-        </tbody>
-      </table>
+      <div className={classes.tableContainer}>
+        <TableContainer component={Paper}>
+          <Table aria-label="student response table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>Team</TableCell>
+                <TableCell align="right">
+                  Answer
+                  {`(${gameInfo.responses.length}/${
+                    Object.values(gameInfo.gameState.students).length
+                  })`}
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {gameInfo.responses.map((student) => (
+                <TableRow key={`${student.name}${student.team}`}>
+                  <TableCell
+                    key={`${student.name}`}
+                    component="th"
+                    scope="row"
+                  >{`${student.name}`}</TableCell>
+                  <TableCell
+                    key={`${student.team}`}
+                  >{`${student.team}`}</TableCell>
+                  <TableCell
+                    key={`${student.response}`}
+                    align="right"
+                  >{`${student.response}`}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
     );
   } else {
     return <div></div>;
